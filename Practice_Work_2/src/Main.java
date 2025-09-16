2cc42929be467118ab6716bb77bf8033af4d3f7f
dmitry-pl <dmitry.pleshko.ggle@mail.com>
2025-09-16 21:16:06 +0300

    PW2_3_arrays_reverse

--- Practice_Work_2/src/Main.java
+++ Practice_Work_2/src/Main.java
@@ -3,19 +3,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        int[] myArray = {1,2,3,4,5,6,7,8,9};
+        int tempNum;
 
-        System.out.println("Введите длину массива: ");
-        int arrayLen = scanner.nextInt();
-
-        int[] intArray = new int[arrayLen];
-        for(int i = 0; i < intArray.length; i++){
-            intArray[i] = random.nextInt(arrayLen + 1);
+        System.out.println("Массив до реверса:");
+        for(int i:myArray){
+            System.out.print(i + " ");
         }
 
-        for(int i: intArray){
-            System.out.println(i);
+        for(int i = 0; i < myArray.length / 2.0; i++){
+            tempNum = myArray[i];
+            myArray[i] = myArray[myArray.length - i - 1];
+            myArray[myArray.length - i - 1] = tempNum;
+            //System.out.print(i + " ");
+        }
+
+        System.out.println("\nМассив после реверса:");
+        for(int i:myArray){
+            System.out.print(i + " ");
         }
     }
 }
