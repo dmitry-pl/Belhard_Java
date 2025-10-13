b4bb7fa486e14b446ad0e5f66a21bdddb6c9c691
dmitry-pl <dmitry.pleshko.ggle@gmail.com>
2025-10-13 22:50:01 +0300

    HW7_in process

--- HomeWork7/Task4_Regex/src/Main.java
+++ HomeWork7/Task4_Regex/src/Main.java
@@ -1,9 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /*
 4.	Дана строка 'ab abab abab abababab abea'. Напишите регулярное выражение,
     которое найдет строки по шаблону: строка 'ab' повторяется 1 или более раз.
  */
 public class Main {
     public static void main(String[] args) {
-
+        String forFind = "ab abab abab abababab abea";
+        String regex = "(ab)+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(forFind);
+        while (matcher.find()) {
+            System.out.println("Подстрока соответствует - " + matcher.group());
+        }
     }
 }
